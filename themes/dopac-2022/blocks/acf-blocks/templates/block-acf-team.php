6d054587a13a5d05373abf431660d9c3947dcdb9
Matt Whiteley <matt@whiteleydesigns.com>
2021-11-08 09:59:51 -0500

    Team / Staff Block

--- themes/dopac-2022/blocks/acf-blocks/templates/block-acf-team.php
+++ themes/dopac-2022/blocks/acf-blocks/templates/block-acf-team.php
@@ -45,7 +45,7 @@ if ( ! empty( $block['className'] ) ) {
                     $wd_team_name = get_sub_field( 'wd_team_name' );
                     $wd_team_headshot = get_sub_field( 'wd_team_headshot' );
                     $wd_team_position = get_sub_field( 'wd_team_position' );
-                    $wd_team_email = get_sub_field( 'wd_team_email' );
+                    $team_member_link = get_sub_field( 'team_member_link' );
                     ?>
 
                     <div class="team-block__item">
@@ -56,7 +56,7 @@ if ( ! empty( $block['className'] ) ) {
                               </div>
                          <?php endif; ?>
 
-                         <h3 class="team-block__name"><?php echo $wd_team_name; ?></h3>
+                         <span class="team-block__name"><?php echo $wd_team_name; ?></span>
 
                          <?php if ( $wd_team_position ) : ?>
                               <span class="team-block__position">
@@ -64,10 +64,10 @@ if ( ! empty( $block['className'] ) ) {
                               </span>
                          <?php endif; ?>
 
-                         <?php if ( $wd_team_email ) : ?>
-                              <span class="team-block__email">
-                                   <a href="mailto:<?php echo $wd_team_email; ?>"><i class="fas fa-paper-plane"></i> Email <?php echo $wd_team_name; ?></a>
-                              </span>
+                         <?php if ( $team_member_link ) : ?>
+                              <div class="team-block__link">
+                                   <a href="<?php echo esc_url( $team_member_link['url'] ); ?>" target="<?php echo esc_attr( $team_member_link['target'] ); ?>">Learn More <i class="fal fa-angle-double-right"></i></a>
+                              </div>
                          <?php endif; ?>
 
                          <?php if ( have_rows( 'wd_team_social' ) ) : ?>
